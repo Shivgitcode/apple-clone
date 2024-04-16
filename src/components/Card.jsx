38cf442ca10251bca9030a7303dc94afa1df593c
shivansh <shivneeraj2004@gmail.com>
2024-04-16 21:51:53 +0530

    making some more changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
+import { watchUltra } from "../assets";
+
 function Card({ el }) {
   console.log(el.name);
   return (
     <div className="relative flex flex-col items-center">
       <div className="flex flex-col justify-center items-center absolute w-full mx-auto py-[53px] gap-y-[10px]">
         {el.name.includes("asset") ? (
-          <img src={el.name} />
+          <img src={watchUltra} />
         ) : (
           <h1
             className={`text-[40px] leading-[1.1] font-semibold ${el.name.includes("AirPods") && "text-white"
